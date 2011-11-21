d601a39a90c44a3dae7354a7d1d4e5b5e6ecb061
liemmaster1990 <liemmaster1990@2f0572ee-e847-9d8e-a4d8-d4a1e3e0d698>
2011-11-21 18:09:20 +0000

    Sửa lại Database

---
+++ trunk/Database/ReportQuery.sql
@@ -0,0 +1,20 @@
+﻿USE NhatQuang
+GO
+
+SET dateformat dmy
+GO
+---/*
+---Tính lợi nhuận bán hàng
+---*/
+SELECT     tbl_PhieuNhapXuat.PK_sPhieuNhapXuatID, tbl_PhieuNhapXuat.dNgayNhapXuat, tbl_HangHoa.PK_iHangHoaID, tbl_ChiTietPhieuNhapXuat.iSoLuong, 
+                      tbl_ChiTietPhieuNhapXuat.fGiaGoc, tbl_ChiTietPhieuNhapXuat.fGiaBan, tbl_ChiTietPhieuNhapXuat.fGiaBan*tbl_ChiTietPhieuNhapXuat.iSoLuong as fThanhTien, tbl_ChiTietPhieuNhapXuat.iSoLuong*(tbl_ChiTietPhieuNhapXuat.fGiaBan-tbl_ChiTietPhieuNhapXuat.fGiaGoc) as fLoiNhuan
+FROM         tbl_PhieuNhapXuat INNER JOIN
+                      tbl_ChiTietPhieuNhapXuat ON tbl_PhieuNhapXuat.PK_sPhieuNhapXuatID = tbl_ChiTietPhieuNhapXuat.FK_sPhieuNhapXuatID INNER JOIN
+                      tbl_HangHoa ON tbl_ChiTietPhieuNhapXuat.FK_iHangHoaID = tbl_HangHoa.PK_iHangHoaID
+WHERE tbl_PhieuNhapXuat.iTrangThai = 1 AND datepart(mm,tbl_PhieuNhapXuat.dNgayNhapXuat) = datepart(mm,getdate())
+
+--!
+--Tách tháng từ ngày tháng
+--select datepart(mm,getdate())
+--*/
+SELECT * FROM tbl_ThongTinCongTy
